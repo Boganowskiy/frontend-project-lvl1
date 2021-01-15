7f0a9009b187f0df8023cef9e10348c686b46761
Boganowskiy Andrey <boganowskiy@gmail.com>
2021-01-15 20:31:04 +0300

    refactored main logic, added index.js, updated src/brainEven.js

--- src/brainEven.js
+++ src/brainEven.js
@@ -1,35 +1,22 @@
-import getUserAnswer from './cli.js';
-import brainGames from './brainGames.js';
+import game from './index.js';
 
 const isEven = (num) => num % 2 === 0;
 
 const getRandomNumber = (max) => Math.floor(Math.random() * Math.floor(max));
 
-const game = () => {
-  const maxNumber = 100;
-  const randomNumber = getRandomNumber(maxNumber);
-  const userAnswer = getUserAnswer(`Question: ${randomNumber} `);
-  console.log(`Your answer: ${userAnswer}`);
-  const correctAnswer = (isEven(randomNumber)) ? 'yes' : 'no';
-  if (userAnswer === correctAnswer) {
-    console.log('Correct!');
-  } else {
-    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'`);
-    return 'failed';
-  }
-  return 'success';
-};
+const getCorrectAnswer = (number) => (isEven(number) ? 'yes' : 'no');
 
-const isGameFinishedFailed = () => game() !== 'success';
+const greeting = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 export default () => {
-  const username = brainGames();
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  for (let i = 0; i <= 2; i += 1) {
-    if (isGameFinishedFailed()) {
-      console.log(`Let's try again, ${username}!`);
-      return;
-    }
+  const maxNumber = 100;
+  const numOfRounds = 3;
+  const questions = [];
+  const correctAnswers = [];
+  for (let i = 0; i < numOfRounds; i += 1) {
+    const number = getRandomNumber(maxNumber);
+    questions.push(number);
+    correctAnswers.push(getCorrectAnswer(number));
   }
-  console.log(`Congratulations, ${username}!`);
+  game(greeting, questions, correctAnswers, numOfRounds);
 };
