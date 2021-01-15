addf2a8baef3e1b6c57e4be25e34956e4695ab92
Boganowskiy Andrey <boganowskiy@gmail.com>
2021-01-15 16:02:17 +0300

    updated src/cli.js and bin/brain-games

--- src/cli.js
+++ src/cli.js
@@ -1,17 +1,8 @@
 import readlineSync from 'readline-sync';
 
-<<<<<<< HEAD
 const getUserAnswer = (questionText) => {
   const answer = readlineSync.question(questionText);
   return answer;
 };
 
 export default getUserAnswer;
-=======
-const getUserName = () => {
-  const name = readlineSync.question('May I have your name? ');
-  return name;
-};
-
-export default getUserName;
->>>>>>> e7fc48226e815b245217aa3fd845bfcab9ab5e8b
