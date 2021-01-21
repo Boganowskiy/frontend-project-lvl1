32550594dbf7fc4a1b0da420cac0e684406298bb
Boganowskiy Andrey <boganowskiy@gmail.com>
2021-01-21 14:02:13 +0300

    added func that return random number between min and max values in getRandomNumber module

--- src/getRandomNumber.js
+++ src/getRandomNumber.js
@@ -1,1 +1,6 @@
-export default (max) => Math.floor(Math.random() * Math.floor(max));
+export const getRandomNumber = (max) => Math.floor(Math.random() * Math.floor(max));
+export const getRandomNumberMinMax = (min, max) => {
+  const roundMin = Math.ceil(min);
+  const roundMax = Math.floor(max);
+  return Math.floor(Math.random() * (roundMax - roundMin)) + roundMin;
+};
